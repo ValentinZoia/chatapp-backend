cab879467a3a750dac0f582fb723fb83e1e27ce0
ValentinZoia <valentinzoia@gmail.com>
2025-09-26 19:54:24 -0300

    feat(add declare namespace for nevs and user in request )

---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+//NodeJS.ProcessEnv
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    USER_DB: string;
+    PASSWORD_DB: string;
+    DB_NAME: string;
+
+    DATABASE_URL: string;
+
+    PORT: number;
+    APP_URL: string;
+
+    JWT_REFRESH_SECRET: string;
+    JWT_ACCESS_SECRET: string;
+
+    IMAGE_PATH: string;
+  }
+}
