5c793daeacce98e459f5ac0db74fdae6ae191db1
ValentinZoia <valentinzoia@gmail.com>
2025-11-07 22:12:34 +0000

    feat(token.service): add token.service

---
+++ src/common/token/token.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { ErrorManager } from '@/src/common/utils/error.manager';
+import { verify } from 'jsonwebtoken';
+
+@Injectable()
+export class TokenService {
+  constructor(private readonly configService: ConfigService) { }
+
+  extractToken(connectionParams: any): string | null {
+    return connectionParams?.token || null;
+  }
+
+  validateToken(token: string) {
+    const refreshTokenSecret = this.configService.get('JWT_REFRESH_SECRET');
+
+    try {
+      return verify(token, refreshTokenSecret);
+    } catch (error) {
+      throw ErrorManager.createSignatureError(error.message);
+    }
+  }
+}
