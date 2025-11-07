2eafaa6c30f839cfa378edc9062085d58788953b
ValentinZoia <valentinzoia@gmail.com>
2025-11-07 22:29:29 +0000

    feat(ProcessEnv) add new var envs

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,21 +2,38 @@
 
 declare namespace NodeJS {
   export interface ProcessEnv {
-    USER_DB: string;
-    PASSWORD_DB: string;
-    DB_NAME: string;
+    NODE_ENV: string;
+    PORT: number;
+    APP_NAME: string;
+    APP_URL: string;
+
+    LOG_LEVEL: string;
+
+    POSTGRES_HOST: string;
+    POSTGRES_PORT: number;
+    POSTGRES_USER: string;
+    POSTGRES_PASSWORD: string;
+    POSTGRES_DB_NAME: string;
 
     DATABASE_URL: string;
 
-    PORT: number;
-    APP_URL: string;
-
     JWT_REFRESH_SECRET: string;
+    JWT_REFRESH_EXPIRATION: string;
+
     JWT_ACCESS_SECRET: string;
+    JWT_ACCESS_EXPIRATION: string;
 
     IMAGE_PATH: string;
 
     REDIS_HOST: string;
     REDIS_PORT: string;
+    REDIS_PASSWORD: string;
+    REDIS_DB: string;
+    REDIS_TLS: boolean;
+
+    CORS_ORIGIN: string;
+
+    THROTTLE_TTL: number;
+    THROTTLE_LIMIT: number;
   }
 }
