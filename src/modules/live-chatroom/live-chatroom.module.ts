7979d084a59b2dd8ae50d641f32423dac545c1f2
ValentinZoia <valentinzoia@gmail.com>
2025-11-07 21:57:06 +0000

    feat(live-chatroom.module): add live-chatroom.module

---
+++ src/modules/live-chatroom/live-chatroom.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { LiveChatroomService } from './services/live-chatroom.service';
+import { LiveChatroomResolver } from './resolvers/live-chatroom.resolver';
+import { UserService } from '@/src/modules/user/services/user.service';
+import { PrismaService } from '@/src/common/prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import { ChatroomService } from 'src/modules/chatroom/services/chatroom.service';
+
+@Module({
+  providers: [
+    LiveChatroomService,
+    LiveChatroomResolver,
+    UserService,
+    PrismaService,
+    JwtService,
+    ChatroomService,
+  ],
+})
+export class LiveChatroomModule { }
