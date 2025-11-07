436602c1061f75af0893583c3799ccf34da782f1
ValentinZoia <valentinzoia@gmail.com>
2025-11-07 21:59:11 +0000

    feat(chatroom.access): add chatroom.access constants

---
+++ src/modules/chatroom/constants/chatroom.access.ts
@@ -0,0 +1,12 @@
+// import { registerEnumType } from '@nestjs/graphql';
+// import { ChatroomAccess } from '@prisma/client';
+
+// export enum ChatroomAccess {
+//   PUBLIC = 'PUBLIC',
+//   PRIVATE = 'PRIVATE',
+// }
+
+// registerEnumType(ChatroomAccess, {
+//   name: 'ChatroomAccess', // este nombre aparecerá en el schema
+//   description: 'Tipos de acceso de la sala de chat',
+// });
